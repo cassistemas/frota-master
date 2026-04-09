ec18bfe985e29c1e22c7a5e72bd6ac4fad7b18e2
cassistemas <castelefonia@gmail.com>
2026-04-09 09:40:24 -0300

    Update paginacao.js

--- paginacao.js
+++ paginacao.js
@@ -150,8 +150,13 @@ function renderModulo(modulo) {
     renderPaginacao('manutencoes','paginacaoManutencoes');
 }
 
-    if(modulo === 'multas'){
-        const dados = getDadosPaginados('multas');
+   let base = multasFiltradas.length ? multasFiltradas : db.multas;
+
+const pagina = obterPagina('multas');
+const inicio = (pagina - 1) * PAGINACAO.itensPorPagina;
+const fim = inicio + PAGINACAO.itensPorPagina;
+
+const dados = base.slice(inicio, fim);
 
         document.getElementById('listaMultas').innerHTML =
         dados.map((mu,i)=>{
@@ -172,7 +177,16 @@ function renderModulo(modulo) {
             </tr>
             `;
         }).join('');
+       let total = base.reduce((soma, m) => {
+    return soma + moedaParaFloat(m.muvalor);
+}, 0);
 
+document.getElementById('listaMultas').innerHTML += `
+<tr style="font-weight:bold;background:#f1f5f9">
+<td colspan="5">TOTAL</td>
+<td colspan="3">${floatParaMoeda(total)}</td>
+</tr>
+`;
         renderPaginacao('multas','paginacaoMultas');
     }
 }
